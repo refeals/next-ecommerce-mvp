d90359f7c3e34179dff0513d1401c354c4ba34d2
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2024-03-27 15:58:23 -0300

    feat: add admin dashboard and products pages

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import {
+  deleteProduct,
+  toggleProductAvailability,
+} from "@/app/admin/_actions/products"
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { useRouter } from "next/navigation"
+import { useTransition } from "react"
+
+export function ActiveToggleDropdownItem({
+  id,
+  isAvailableForPurchase,
+}: {
+  id: string
+  isAvailableForPurchase: boolean
+}) {
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+
+  return (
+    <DropdownMenuItem
+      disabled={isPending}
+      onClick={() => {
+        startTransition(async () => {
+          await toggleProductAvailability(id, !isAvailableForPurchase)
+          router.refresh()
+        })
+      }}
+    >
+      {isAvailableForPurchase ? "Deactivate" : "Activate"}
+    </DropdownMenuItem>
+  )
+}
+
+export function DeleteDropdownItem({
+  id,
+  disabled,
+}: {
+  id: string
+  disabled: boolean // cant delete if product has orders
+}) {
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
+
+  return (
+    <DropdownMenuItem
+      variant="destructive"
+      disabled={disabled || isPending}
+      onClick={() => {
+        startTransition(async () => {
+          await deleteProduct(id)
+          router.refresh()
+        })
+      }}
+    >
+      Delete
+    </DropdownMenuItem>
+  )
+}
