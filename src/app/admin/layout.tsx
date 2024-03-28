ce336adbc946365a671a12e8acd713ee9c22170f
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2024-03-28 11:06:32 -0300

    feat: add stripe functionality and admin/users page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -12,7 +12,7 @@ export default function AdminLayout({
       <Nav>
         <NavLink href="/admin">Dashboard</NavLink>
         <NavLink href="/admin/products">Products</NavLink>
-        <NavLink href="/admin/users">Users</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
         <NavLink href="/admin/orders">Orders</NavLink>
       </Nav>
       <div className="container my-6">{children}</div>
