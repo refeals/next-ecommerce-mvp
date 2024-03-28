8783aca386219bad07fab631debffedf66ab2c7c
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2024-03-28 11:21:38 -0300

    feat: add admin orders page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,7 +13,7 @@ export default function AdminLayout({
         <NavLink href="/admin">Dashboard</NavLink>
         <NavLink href="/admin/products">Products</NavLink>
         <NavLink href="/admin/users">Customers</NavLink>
-        <NavLink href="/admin/orders">Orders</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
       </Nav>
       <div className="container my-6">{children}</div>
     </>
